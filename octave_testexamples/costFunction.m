e0689e70748c54c36b68bc602b9c647098221d17
Qhan <qhan555@163.com>
2018-11-24 06:33:49 -0500

    Finish ex4

---
+++ octave_testexamples/costFunction.m
@@ -0,0 +1,9 @@
+function J=costFunction(X,y,theta)
+% X is the 'design matrix' containing our training examples.
+% y is the class labels.
+
+m=size(X,1); % number of training examples
+predictions=X*theta; %predictions of hypothesis on all training examples
+sqrErrors=(predictions-y).^2; % squared errors
+J= 1./(2.*m) * sum(sqrErrors);
+
